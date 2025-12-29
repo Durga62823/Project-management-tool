187cc594c6eea0ec4eb82fe5d7a2d71a64654f7e
Durga prasad <plakshmi8080@gmail.com>
2025-12-29 11:48:23 +0530

    unwanted data is deleted

--- prisma/migrations/20251203085132_add_user_preferences/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "notificationSettings" JSONB DEFAULT '{"emailNotifications":true,"projectUpdates":true,"teamActivity":true,"loginAlerts":true}',
-ADD COLUMN     "preferences" JSONB DEFAULT '{"theme":"light","language":"English","timezone":"UTC"}';
